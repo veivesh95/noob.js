493de8386e788e62b5478a8eff167dd16e532b42
3LOGIXS\seetharam_s <satheeskumar.s@eyepax.com>
2019-02-24 04:14:17 +0530

    angular 32

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { PostViewComponent } from './post-view/post-view.component';
 import { AddCommentComponent } from './add-comment/add-comment.component';
 import { AddPostComponent } from './add-post/add-post.component';
 import { LoginComponent } from './login/login.component';
-import { RantListComponent } from './rant-list/rant-list.component';
+import { RantComponent } from './rant/rant.component';
 
 const routes: Routes = [
-  { path: '', component: RantListComponent },
+  { path: '', component: RantComponent },
   { path: 'post/id', component: PostViewComponent },
   { path: 'add/comment', component: AddCommentComponent },
   { path: 'add/post', component: AddPostComponent },
