1fee766a4444e161ff1a805d095c362a27a64a1c
veivesh <veivesh95@gmail.com>
2019-02-23 15:42:53 +0530

    react-7

---
+++ react/src/components/elements/Spinner.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export const Spinner = () => {
+  return (
+    <Fragment>
+      <div className="loader">
+        <div className="spinner" />
+      </div>
+    </Fragment>
+  );
+};
